7e686bb6e402e084a644d75bc8eaaee2361c513b
Kamil <kazmierczak.kamil1996@gmail.com>
2021-08-02 17:28:48 +0200

    Added hbs

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+const express = require('express')
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/kontakt', (req, res) => {
+    res.render('kontakt')
+})
+
+app.listen(port)
+
 const utils = require("./utils")
 utils.greeting()
 utils.add(4, 5)
+
